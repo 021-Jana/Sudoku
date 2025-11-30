a335f489d1114212d539cd24d9facf993f846f82
Jana Saeed <janasaeed214@gmail.com>
2025-11-30 02:07:50 +0200

    Create ValidatorFactory.java

---
+++ ValidatorFactory.java
@@ -0,0 +1,10 @@
+
+package sudoku;
+
+public class ValidatorFactory {
+
+    static Validator createValidator(int mode, Board board) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
